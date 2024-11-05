551e9d699721621d346f4e5e13fe1c1a06402509
Gonglin Chen <47229849+xtcpete@users.noreply.github.com>
2024-11-05 14:27:47 -0800

    fix error

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+//vue+vite boiler-plate css
+import './assets/main.css'
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
